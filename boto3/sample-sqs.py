79c8903672b1f678b52058be0b75617123f496a5
reshma-a84 <31404305+reshma-a84@users.noreply.github.com>
2022-10-08 00:06:22 +0530

    All commit

---
+++ boto3/sample-sqs.py
@@ -0,0 +1,9 @@
+from inspect import Attribute
+import boto3
+
+sqs = boto3.resource('sqs')
+
+queue = sqs.create_queue(QueueName= 'test-boto3-sqs', Attributes= {'DelaySeconds': '5'})
+
+print(queue.url)
+print(queue.attributes.get('DelaySeconds'))
